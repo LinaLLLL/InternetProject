2007922b04e71782b82ad3b9b9bcd7e4f1c0fdc3
lina1 <lina1@inetVesh.myguest.virtualbox.org>
2025-01-15 22:44:28 +0500

    Добавлена функция для воспроизведения звука

--- cat_scary.py
+++ cat_scary.py
@@ -5,4 +5,8 @@ import time
 # Инициализация библиотеки pygame для воспроизведения звука
 pygame.mixer.init()
 
+# Функция для воспроизведения звука
+def play_sound():
+    pygame.mixer.music.load("/home/lina1/Desktop/project/claymore_ringtone.mp3")  # путь к звуковому файлу
+    pygame.mixer.music.play()
 
