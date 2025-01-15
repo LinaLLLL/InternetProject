b0888a1516e86283d0ea463bec2a06ea9c25b168
lina1 <lina1@inetVesh.myguest.virtualbox.org>
2025-01-15 22:50:15 +0500

    Добавлена загрузка классификатора для определения кошек

--- cat_scary.py
+++ cat_scary.py
@@ -10,3 +10,7 @@ def play_sound():
     pygame.mixer.music.load("/home/lina1/Desktop/project/claymore_ringtone.mp3")  # путь к звуковому файлу
     pygame.mixer.music.play()
 
+# Загрузка классификатора для обнаружения объектов (котов)
+cat_cascade = cv2.CascadeClassifier("/home/lina1/Desktop/project/haarcascade_frontalcatface.xml")
+if cat_cascade.empty():
+	print("ошибка загрузки каскада. проверьте путь и файл")
